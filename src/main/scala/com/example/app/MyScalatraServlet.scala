f627a77618166b2ef7ea9fdc251f16b0972e0d08
sany0k <sanyokw@gmail.com>
2013-04-24 15:40:31 +0400

    init

---
+++ src/main/scala/com/example/app/MyScalatraServlet.scala
@@ -0,0 +1,17 @@
+package com.example.app
+
+import org.scalatra._
+import scalate.ScalateSupport
+
+class MyScalatraServlet extends ScalatraStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!342dfgdfg</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+  
+}
